658b1c8d41559c7b850c505143b40177ff0cf622
ankur <ankurjana905@gmail.com>
2023-03-28 18:43:09 +0530

    update payment address

--- src/main/kotlin/com/yantribits/eCommerse/model/PaymentAddress.kt
+++ src/main/kotlin/com/yantribits/eCommerse/model/PaymentAddress.kt
@@ -1,13 +1,27 @@
 package com.yantribits.eCommerse.model
 
 import com.yantribits.eCommerse.model.enums.DataStatus
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import java.time.ZonedDateTime
 import java.util.UUID
 
+@Entity(name = "payment_address")
 data class PaymentAddress(
-    var id:String=UUID.randomUUID().toString(),
+    @Id
+    var id: String = UUID.randomUUID().toString(),
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     var dataStatus: DataStatus = DataStatus.ACTIVE,
-    var createdDate: CreatedDate = CreatedDate(),
-    var modifiedDate: LastModifiedDate = LastModifiedDate()
+    @CreationTimestamp
+    var createdDate: ZonedDateTime = ZonedDateTime.now(),
+    @UpdateTimestamp
+    var modifiedDate: ZonedDateTime = ZonedDateTime.now()
 )
