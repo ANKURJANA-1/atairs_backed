51b2dce49fc1cd75942d78be438e8b35ec17434b
ankur <ankurjana905@gmail.com>
2023-03-28 18:12:45 +0530

    update access control

--- src/main/kotlin/com/yantribits/eCommerse/model/AccessControl.kt
+++ src/main/kotlin/com/yantribits/eCommerse/model/AccessControl.kt
@@ -2,19 +2,35 @@ package com.yantribits.eCommerse.model
 
 import com.yantribits.eCommerse.model.enums.DataStatus
 import com.yantribits.eCommerse.model.enums.MethodType
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.util.ArrayList
 import java.util.UUID
 
+
+@Entity(name = "access_controls")
 data class AccessControl(
+    @Id
     var id: String = UUID.randomUUID().toString(),
+    @Column(nullable = false, unique = true)
     var name: String = "",
+    @Column(nullable = false)
     var desc: String = "",
+    @Column(nullable = false, unique = true)
     var title: String = "",
+    @Column(nullable = false, unique = true)
     var methodName: String = "",
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     var methodType: MethodType = MethodType.NOT_DEFINE,
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     var dataStatus: DataStatus = DataStatus.ACTIVE,
+    @CreationTimestamp
     var createdDate: CreatedDate = CreatedDate(),
+    @UpdateTimestamp
     var modifiedDate: LastModifiedDate = LastModifiedDate()
 )
